b1890a3d62c96d506754407d5ef83e504ff641a0
Imrul Emon <emon_wb@Imruls-Mac-mini.local>
2022-10-30 15:30:45 +0600

    Todo App Bar

---
+++ android/app/src/main/kotlin/com/example/todo_wb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_wb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+import 'package:todo_wb/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/avatar.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        child: Text('This is home screen'),
+      ),
+    );
+  }
+}
